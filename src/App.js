02eb1709e827087019499e9a4b4af05685571987
GlauberCardoso <glauberdevs@gmail.com>
2022-11-16 13:41:26 -0300

    Migra HTML do componente da página inicial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Home from "./components/pages/Home";
+
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+import "./styles/main.css"
+
 function App() {
   return (
-    <div>Projeto</div>
+    <Home />
   );
 }
 
