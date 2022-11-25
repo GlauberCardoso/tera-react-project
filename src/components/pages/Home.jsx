66995979f3cf148e621070637e5f1584e8c0895b
GlauberCardoso <glauberdevs@gmail.com>
2022-11-25 12:09:21 -0300

    Create molecules and organisms

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,29 +1,56 @@
-//import React from "react";
-//import logo from "../../images/logo.svg";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import AppLoading from "../organisms/AppLoading";
+
+import logo from "../../images/logo.svg";
 
 export default function Home() {
-  /*const [users, setUsers] = React.useState([]);
-  const [currentuser, setCurrentUser] = React.useState("");
+  const navigate = useNavigate();
 
-  const handleUserChange = () => {
-    console.log("handleUserChange")
-  }
+  const [users, setUsers] = React.useState([]);
+  const [currentUser, setCurrentUser] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://62c4e487abea8c085a7e022a.mockapi.io/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleUserChange = (event) => {
+    setCurrentUser(event.target.value);
+  };
 
   const handleConfirmClick = () => {
-    console.log("handleConfirmClick")
-  }
+    navigate(`/users/${currentUser}`);
+  };
 
-  return (
+  return isLoading ? (
+    <AppLoading />
+  ) : (
     <div className="home center">
       <div className="home__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
       <select onChange={handleUserChange} className="home__select-users">
-        <option>User 1</option>
-        <option>User 2</option>
-        <option>User 3</option>
+        <option value="">Selecionar Usuário</option>
+        {users
+          .sort((a, b) => a.fn.localeCompare(b.fn))
+          .map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.fn} {user.ln}
+            </option>
+          ))}
       </select>
-      <button onClick={handleConfirmClick} className="button-primary">Entrar</button>
+      {!!currentUser && (
+        <button onClick={handleConfirmClick} className="button-primary">
+          Entrar
+        </button>
+      )}
     </div>
-  );*/
+  );
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,50 +1,46 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
 
 import React from "react";
+import { useParams } from "react-router-dom";
 
-import logo from "../../images/logo.svg";
-import user1 from "../../images/placeholders/user-1.jpg";
-import user2 from "../../images/placeholders/user-2.jpg";
-import user3 from "../../images/placeholders/user-3.jpg";
+import Default from "../templates/Default";
+import AppLoading from "../organisms/AppLoading";
 
-export default function Users() {
-  return (
-    <div className="wrapper">
-      <div className="app-header">
-        <div className="app-header__logo">
-          <img src={logo} className="responsive" alt="" />
-        </div>
-        <div className="app-header__menu">
-          <i className="fa fa-bars"></i>
-        </div>
-      </div>
+// import user1 from "../../images/placeholders/user-1.jpg";
+// import user2 from "../../images/placeholders/user-2.jpg";
+// import user3 from "../../images/placeholders/user-3.jpg";
 
+export default function Users(props) {
+  const { userId } = useParams();
+
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch(`https://62c4e487abea8c085a7e022a.mockapi.io/users/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, [userId]);
+
+  return isLoading ? (
+    <AppLoading />
+  ) : (
+    <Default>
       <div className="users">
         <h1>Users</h1>
 
         <div className="users__list">
           <a href="#" className="users__list-item">
             <div className="users__list-item-photo">
-              <img src={user1} className="responsive avatar" alt="" />
+              <img src={users} className="responsive avatar" alt="" />
             </div>
-            <div className="users__list-item-name">John Walter</div>
-          </a>
-          <a href="#" className="users__list-item">
-            <div className="users__list-item-photo">
-              <img src={user2} className="responsive avatar" alt="" />
-            </div>
-            <div className="users__list-item-name">Chris Hudson</div>
-          </a>
-          <a href="#" className="users__list-item">
-            <div className="users__list-item-photo">
-              <img src={user3} className="responsive avatar" alt="" />
-            </div>
-            <div className="users__list-item-name">Bryan Cornell</div>
+            <div className="users__list-item-name">{props.users}</div>
           </a>
         </div>
       </div>
-
-      <div className="app-footer">Módulo React :: Full Stack Development</div>
-    </div>
+    </Default>
   );
 }
